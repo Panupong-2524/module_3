52ad50691e1031ad63074c49f294b52aa4f386a9
Panupong-2524 <sanprasit.panupong@gmail.com>
2025-04-07 19:42:53 +0700

    - Revert code change to Main branch

--- petstore/petstoreorderservice/src/main/java/com/chtrembl/petstore/order/api/StoreApiController.java
+++ petstore/petstoreorderservice/src/main/java/com/chtrembl/petstore/order/api/StoreApiController.java
@@ -3,7 +3,6 @@ package com.chtrembl.petstore.order.api;
 import com.chtrembl.petstore.order.model.ContainerEnvironment;
 import com.chtrembl.petstore.order.model.Order;
 import com.chtrembl.petstore.order.model.Product;
-import com.chtrembl.petstore.order.service.OrderService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
@@ -40,9 +39,6 @@ public class StoreApiController implements StoreApi {
 	private final ObjectMapper objectMapper;
 
 	private final NativeWebRequest request;
-
-	@Autowired
-	private OrderService orderService;
 
 	@Autowired
 	@Qualifier(value = "cacheManager")
@@ -118,21 +114,19 @@ public class StoreApiController implements StoreApi {
 					"PetStoreOrderService incoming POST request to petstoreorderservice/v2/order/placeOder for order id:%s",
 					body.getId()));
 
-			com.chtrembl.petstore.order.entity.Order order = orderService.getOrderById(body.getId());
-			order.setId(body.getId());
-			order.setOrderId(body.getId());
-			order.setEmail(body.getEmail());
-			order.setComplete(body.isComplete());
+			this.storeApiCache.getOrder(body.getId()).setId(body.getId());
+			this.storeApiCache.getOrder(body.getId()).setEmail(body.getEmail());
+			this.storeApiCache.getOrder(body.getId()).setComplete(body.isComplete());
 
 			// 1 product is just an add from a product page so cache needs to be updated
 			if (body.getProducts() != null && body.getProducts().size() == 1) {
 				Product incomingProduct = body.getProducts().get(0);
-				List<Product> existingProducts = order.getProducts();
+				List<Product> existingProducts = this.storeApiCache.getOrder(body.getId()).getProducts();
 				if (existingProducts != null && existingProducts.size() > 0) {
 					// removal if one exists...
 					if (incomingProduct.getQuantity() == 0) {
 						existingProducts.removeIf(product -> product.getId().equals(incomingProduct.getId()));
-						order.setProducts(existingProducts);
+						this.storeApiCache.getOrder(body.getId()).setProducts(existingProducts);
 					}
 					// update quantity if one exists or add new entry
 					else {
@@ -151,25 +145,24 @@ public class StoreApiController implements StoreApi {
 							}
 						} else {
 							// existing products but one does not exist matching the incoming product
-							order.getProducts().add(body.getProducts().get(0));
+							this.storeApiCache.getOrder(body.getId()).addProductsItem(body.getProducts().get(0));
 						}
 					}
 				} else {
 					// nothing existing....
 					if (body.getProducts().get(0).getQuantity() > 0) {
-						order.setProducts(body.getProducts());
+						this.storeApiCache.getOrder(body.getId()).setProducts(body.getProducts());
 					}
 				}
 			}
 			// n products is the current order being modified and so cache can be replaced
 			// with it
 			if (body.getProducts() != null && body.getProducts().size() > 1) {
-				order.setProducts(body.getProducts());
+				this.storeApiCache.getOrder(body.getId()).setProducts(body.getProducts());
 			}
 
 			try {
-				log.info("Save cosmos order");
-				orderService.createOrder(order);
+				Order order = this.storeApiCache.getOrder(body.getId());
 				String orderJSON = new ObjectMapper().writeValueAsString(order);
 
 				ApiUtil.setResponse(request, "application/json", orderJSON);
@@ -200,7 +193,7 @@ public class StoreApiController implements StoreApi {
 
 			List<Product> products = this.storeApiCache.getProducts();
 
-			com.chtrembl.petstore.order.entity.Order order = orderService.getOrderById(orderId);
+			Order order = this.storeApiCache.getOrder(orderId);
 
 			if (products != null) {
 				// cross reference order data (order only has product id and qty) with product
@@ -213,7 +206,6 @@ public class StoreApiController implements StoreApi {
 							p.setPhotoURL((peekedProduct.getPhotoURL()));
 						}
 					}
-					orderService.createOrder(order);
 				} catch (Exception e) {
 					log.error(String.format(
 							"PetStoreOrderService incoming GET request to petstoreorderservice/v2/order/getOrderById for order id:%s failed: %s",
